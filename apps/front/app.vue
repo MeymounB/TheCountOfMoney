d5b20ba5a38d4130efc1f205d0f4d51832a23fac
kieran <kieran.boudouin@epitech.eu>
2023-11-14 11:15:07 +0100

    [ADD] Components folder + Navigation Bar

--- apps/front/app.vue
+++ apps/front/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NavigationBar />
   </div>
 </template>
--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
   devtools: { enabled: true },
 
   css: ["@/assets/scss/main.scss"],
@@ -10,4 +16,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+});
