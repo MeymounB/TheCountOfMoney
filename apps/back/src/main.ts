18beccaad5a157088d80302319a6419344f79d5b
Jolan <jolan.brizard@epitech.eu>
2023-12-18 11:15:41 +0100

    hotfix(back): authorized origins env variables

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: JSON.parse(process.env.ALLOWED_ORIGINS),
     credentials: true,
   });
   app.use(cookieParser());
